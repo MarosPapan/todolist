8a26d9846c94db5d585c5cb3f3681a77e71914e3
MarosPapan <65183080+MarosPapan@users.noreply.github.com>
2020-12-17 20:28:24 +0100

    [WIP] adding typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Store from './services/store/index';
-import App from './App'
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Store from "./services/store/index";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <Store>
     <App />
   </Store>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
